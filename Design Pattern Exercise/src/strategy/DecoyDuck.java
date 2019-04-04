ac1a6deaff1cb0a0d3cbb3d1083456c52daf9996
User <User@DESKTOP-OS6G08R>
2019-04-04 15:49:20 +0800

    加入誘餌鴨(DecoyDuck)，不會叫也不會飛，無法通過測試。

---
+++ Design Pattern Exercise/src/strategy/DecoyDuck.java
@@ -0,0 +1,8 @@
+package strategy;
+
+public class DecoyDuck extends Duck {
+	
+	public String display() {
+		return "I'm a duck Decoy";
+	}
+}
---
+++ Design Pattern Exercise/src/strategy/test/DecoyDuckTest.java
@@ -0,0 +1,46 @@
+package strategy.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import strategy.DecoyDuck;
+
+class DecoyDuckTest {
+	
+	DecoyDuck duck;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		duck = new DecoyDuck();
+	}
+
+	@Test
+	void testQuack() {
+		String expected = "I can't quack";
+		String actual = duck.quack();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testSwin() {
+		String expected = "Swin";
+		String actual = duck.swim();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testDisplay() {
+		String expected = "I'm a duck Decoy";
+		String actual = duck.display();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testFly() {
+		String expected = "I can't fly";
+		String actual = duck.fly();
+		assertEquals(expected, actual);
+	}
+}
