e4023ac51f99530d95087d87f5fd60311db83604
User <User@DESKTOP-OS6G08R>
2019-04-04 15:51:50 +0800

    覆寫DecoyDuck的quack和fly方法，使其通過測試。但這顯然不是好方法。

--- Design Pattern Exercise/src/strategy/DecoyDuck.java
+++ Design Pattern Exercise/src/strategy/DecoyDuck.java
@@ -5,4 +5,14 @@ public class DecoyDuck extends Duck {
 	public String display() {
 		return "I'm a duck Decoy";
 	}
+	
+	@Override
+	public String quack() {
+		return "I can't quack";
+	}
+	
+	@Override
+	public String fly() {
+		return "I can't fly";
+	}
 }
